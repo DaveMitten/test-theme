1af42d86609ff14d3c75a565faf4268684855155
DaveMitten <davidmitten88@gmail.com>
2017-10-26 13:35:12 +0100

    finished off header.php and added page.php

--- header.php
+++ header.php
@@ -2,7 +2,12 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title><?php bloginfo('title')?> </title>
+    <title><?php bloginfo('title')?></title>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>"/>
+    <?php wp_head()?>
   </head>
+  <header>
+    <h1>
+      <a href="<?php home_url('/') ?>"><?php bloginfo('name') ?></h1>
+  </header>
       <div id="container">
