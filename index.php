4847949509f17967f50ff34cb41bf5e57c4eaa4e
DaveMitten <davidmitten88@gmail.com>
2017-10-26 16:40:39 +0100

    tidied ways to use html and php tags

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<h1>hello world</h1>
+<?php
+
+get_header();
+
+if (have_posts()) {
+  while (have_posts()) {
+    the_post();
+    the_title('<h2><a href=<? php the_permalink(); ?>', '</a></h2>' );
+    the_content();
+
+
+  }
+
+} else {
+  echo "<p>No content here, mate!</p>";
+}
+get_footer();
